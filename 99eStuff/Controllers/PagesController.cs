0d835df72e775c2a806b4fe75bc03997006f513f
Andrei Ciprian Vrancianu <vrancianuandrei@gmail.com>
2020-01-23 20:02:01 +0200

    Update footer/header

---
+++ 99eStuff/Controllers/PagesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _99eStuff.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Pages
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+        public ActionResult LoginRegister()
+        {
+            return View();
+        }
+        public ActionResult ProductDetails()
+        {
+            return View();
+        }
+        public ActionResult Wishlist()
+        {
+            return View();
+        }
+    }
+}
